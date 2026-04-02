955ec01ccb7eeb88585365c501bcee781810ddfc
Alexander-Karlsson <Alexanderkarlsson3@gmail.com>
2026-04-02 13:51:17 +0200

    added functionality to read connectionstring and sent DbContextOptions via DI

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -5,11 +5,15 @@ public class Order
     public Guid Id { get; set; }
     public Guid CustomerId { get; set; }
     public Customer Customer { get; set; }
+    
     public int ShippingId { get; set; }
     public Shipping Shipping { get; set; }
+    
     public int ProductId { get; set; }
     public ICollection<Product> Products { get; set; }
+    
     public int PaymentMethodId { get; set; }
     public PaymentMethod PaymentMethod { get; set; }
+    
     public decimal TotalPrice { get; set; }
 }
--- HustlersAB.Admin/Program.cs
+++ HustlersAB.Admin/Program.cs
@@ -1,5 +1,8 @@
 ﻿
-
+using EF_MSSQL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HustlersAB.Admin;
 
@@ -7,7 +10,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var services = new ServiceCollection();
+
+        services.AddDbContext<StoreDbContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
         
+        var serviceProvider = services.BuildServiceProvider();
 
     }
 }
