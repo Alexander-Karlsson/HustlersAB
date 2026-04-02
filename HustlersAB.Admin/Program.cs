9058bd9f57de4ed21f183537399c3a2eeca3907d
Alexander-Karlsson <Alexanderkarlsson3@gmail.com>
2026-04-02 14:07:05 +0200

    made DbContextOptions DI in StoreDbContext and added implementation in Program.cs

--- HustlersAB.Admin/Program.cs
+++ HustlersAB.Admin/Program.cs
@@ -1,13 +1,28 @@
 ﻿
 
 
+using EF_MSSQL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace HustlersAB.Admin;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var services = new ServiceCollection();
+
+        services.AddDbContext<StoreDbContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+        var serviceProvider = services.BuildServiceProvider();
 
     }
 }
