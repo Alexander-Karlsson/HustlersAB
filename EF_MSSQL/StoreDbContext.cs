02a94dd5e589522e1bba2374bdbf82bbfb76f687
Alexander-Karlsson <Alexanderkarlsson3@gmail.com>
2026-04-02 13:52:40 +0200

    Added functionality to read ConnectionString via DI

--- EF_MSSQL/StoreDbContext.cs
+++ EF_MSSQL/StoreDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EF_MSSQL;
 
-public class StoreDbContext(DbContextOptions<StoreDbContext> options) : DbContext(options)
+public class StoreDbContext : DbContext
 {
 
     public DbSet<Product> Products { get; set; }
@@ -15,5 +15,29 @@ public class StoreDbContext(DbContextOptions<StoreDbContext> options) : DbContex
     public DbSet<CustomerContactInfo> CustomerContactInfo { get; set; }
     public DbSet<ProductCategory> ProductCategories { get; set; }
     public DbSet<ProductSubCategory> ProductSubCategories { get; set; }
-    
+
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("ConnectionString");
+    }
+
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+    //    base.OnModelCreating(modelBuilder);
+
+
+
+    //    // modelBuilder.Entity<Product>().HasData(
+    //    //     new Product { Id = Guid.Parse("a1b2c3d4-e5f6-7890-abcd-ef1234567890"), Name = "iPhone", SubCategoryId = 1, SubCategory = },
+    //    //     new Product { Id = Guid.Parse("a1b2c4d6-e5f9-7891-abcd-ef1234567890"), Name = "iPad" },
+    //    //     new Product { Id = Guid.Parse("a1b2c3d4-e5f6-7890-abcd-ef1234565490"), Name = "Mac" }
+    //    // );
+
+    //    // modelBuilder.Entity<ProductCategory>().HasData(
+    //    //     new ProductCategory { Id = Guid.Parse("b1b3r3d4-e5f6-7890-acbd-ef1234565490"), Name = "Apple" }
+    //    // );
+    //}
+
 }
--- HustlersAB.Admin/Program.cs
+++ HustlersAB.Admin/Program.cs
@@ -1,8 +1,5 @@
 ﻿
-using EF_MSSQL;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+
 
 namespace HustlersAB.Admin;
 
@@ -10,16 +7,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var config = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json")
-            .Build();
-
-        var services = new ServiceCollection();
-
-        services.AddDbContext<StoreDbContext>(options =>
-            options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
         
-        var serviceProvider = services.BuildServiceProvider();
 
     }
 }
