04852811d68931f64634f9c34c538a904756ad05
Peter Gergawy <pgergawy@gmail.com>
2026-04-06 10:44:50 +0300

    Only Services/Interfaces

---
+++ Services/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+
+namespace Services.Interfaces;
+
+public interface ICustomerRepository
+{
+    Task<IEnumerable<Customer>> GetAllAsync();
+    Task<Customer?> GetByIdAsync(Guid id);
+    Task<Customer?> GetByEmailAsync(string email);
+    Task<IEnumerable<Customer>> GetByMemberStatusAsync(bool isMember);
+    Task AddAsync(Customer customer);
+    Task UpdateAsync(Customer customer);
+    Task DeleteAsync(Guid id);
+}
