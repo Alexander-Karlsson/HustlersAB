17bb749a0ca5745a76c2a012694f576b23b4e63a
Alexander-Karlsson <Alexanderkarlsson3@gmail.com>
2026-04-08 12:49:55 +0200

    Added content in CustomerService.cs and ICustomerService.cs

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -1,8 +1,28 @@
+using Entities;
 using Services.Interfaces;
 
 namespace Services;
 
 public class CustomerService(ICustomerRepository repo) : ICustomerService
 {
+    public async Task<IEnumerable<Customer>> GetAllAsync()
+        => await repo.GetAllAsync();
     
+    public async Task<Customer?> GetByIdAsync(Guid id)
+     => await repo.GetByIdAsync(id);
+
+    public async Task<IEnumerable<Customer>> GetBySearchAsync(string query)
+        => await repo.GetBySearchAsync(query);
+
+    public async Task<IEnumerable<Customer>> GetMembersAsync()
+        => await repo.GetMembersAsync();
+
+    public async Task<Customer> CreateAsync(Customer customer)
+        => await repo.CreateAsync(customer);
+
+    public async Task UpdateAsync(Customer customer)
+        => await repo.UpdateAsync(customer);
+
+    public async Task DeleteAsync(Guid id)
+        => await repo.DeleteAsync(id);
 }
