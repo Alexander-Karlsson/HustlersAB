489ab17c1f0c89b0000b0946c919d4952b9321a4
Alexander-Karlsson <Alexanderkarlsson3@gmail.com>
2026-04-08 13:18:03 +0200

    Added content to OrderService.cs and IOrderService.cs

--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -1,8 +1,25 @@
+using Entities;
 using Services.Interfaces;
 
 namespace Services;
 
 public class OrderService(IOrderRepository repo) : IOrderService
 {
+    public async Task<IEnumerable<Order>> GetAllAsync()
+        => await repo.GetAllAsync();
     
+    public async Task<Order?> GetByIdAsync(Guid id)
+        => await repo.GetByIdAsync(id);
+
+    public async Task<IEnumerable<Order>> GetBySearchAsync(string query)
+        => await repo.GetBySearchAsync(query);
+    
+    public async Task<Order> CreateAsync(Order order)
+        => await repo.CreateAsync(order);
+
+    public async Task UpdateAsync(Order order)
+        => await repo.UpdateAsync(order);
+
+    public async Task DeleteAsync(Guid id)
+        => await repo.DeleteAsync(id);
 }
