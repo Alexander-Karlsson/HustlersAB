21cdf12cd2373174baac75c282785efe4786c784
Alexander-Karlsson <Alexanderkarlsson3@gmail.com>
2026-04-09 09:20:38 +0200

    Added Admin view, client view, entryPoint and abstract baseMenuclass.

---
+++ HustlersAB.EntryPoint/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HustlersAB.EntryPoint;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        var startMenu = new StartPageMenu();
+        startMenu.Start();
+        
+    }
+}
---
+++ HustlersAB.Shared/Menus/BaseMenu.cs
@@ -0,0 +1,73 @@
+namespace HustlersAB.Shared.Menus;
+
+public abstract class BaseMenu
+{
+        protected int SelectedIndex { get; set; } = 0;
+
+        
+        protected abstract string[] Options { get; }
+        
+        protected virtual string MenuTitle => "MENU";
+
+        // Vad som händer när användaren trycker Enter
+        protected abstract bool ExecuteChoice(int selectedIndex);
+
+        // Starta menyn
+        public void Start()
+        {
+            bool exitMenu = false;
+            while (!exitMenu)
+            {
+                PrintMenu($"---- {MenuTitle} ----");
+                // Console.Clear();
+
+                var key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.Escape)
+                {
+                    exitMenu = true;
+                    continue;
+                }
+                
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        SelectedIndex = (SelectedIndex - 1 + Options.Length) % Options.Length;
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        SelectedIndex = (SelectedIndex + 1) % Options.Length;
+                        break;
+
+                    case ConsoleKey.Enter:
+                        exitMenu = ExecuteChoice(SelectedIndex);
+                        break;
+                }
+            }
+        }
+        
+        private void PrintMenu(string menuName)
+        {
+            Console.Clear();
+            Console.WriteLine("----- HUSTLERS AB ------");
+            Console.WriteLine($"--- {menuName.ToUpperInvariant()} ---\n");
+            
+            for (int i = 0; i < Options.Length; i++)
+            {
+                if (i == SelectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{Options[i]} <-");
+                }
+                else
+                {
+                    Console.ResetColor();
+                    Console.WriteLine(Options[i]);
+                }
+            }
+
+            Console.ResetColor();
+            Console.WriteLine("\nUse ↑/↓ to navigate, Enter to select, Esc to exit");
+        }
+    
+}
