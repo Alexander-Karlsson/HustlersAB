722fe25957884b0834a6c746b37e4b52e9bc2e0c
Fidelilundgren <fideli.lundgren@gmail.com>
2026-04-09 14:05:29 +0200

    Starting on clientmenu

---
+++ HustlersAB.Client/Menus/ClientMenu.cs
@@ -0,0 +1,30 @@
+﻿using HustlersAB.Shared.Menus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HustlersAB.Client.Menus;
+
+public class ClientMenu : BaseMenu
+{
+    protected override string[] Options =>
+        [
+        "Browse Products",
+        "View Cart",
+        "Exit"
+        ];
+
+    protected override bool ExecuteChoice(int selectedIndex)
+    {
+        switch (selectedIndex)
+        {
+            case 0:
+                Console.WriteLine("Browsing products");
+                return false;
+
+            default:
+                break;
+        }
+        return true;
+    }
+}
