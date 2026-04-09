7fe804145ce9fd81d4197e039dc3aca716307381
Alexander-Karlsson <Alexanderkarlsson3@gmail.com>
2026-04-09 21:43:22 +0200

    Basic CRUD added to CustomerService

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -6,13 +6,30 @@ namespace Services;
 
 public class ProductService(IProductRepository repo) : IProductService
 {
-    public async Task<IEnumerable<Product>> GetStartPageProductsAsync()
-    {
-        return await repo.GetStartPageProductsAsync();
-    }
+    public async Task<IEnumerable<Product>> GetAllAsync()
+        => await repo.GetAllAsync();
 
+    public async Task<Product?> GetByIdAsync(Guid id)
+        => await repo.GetByIdAsync(id);
+    
+    public async Task<IEnumerable<Product>> GetBySearchAsync(string query)
+        => await repo.GetBySearchAsync(query);
+    
+    public async Task<IEnumerable<Product>> GetStartPageProductsAsync()
+        => await repo.GetStartPageProductsAsync();
+    
     public async Task<IEnumerable<Product>> GetProductByParentCategoryAsync(string query)
-    {
-        return await repo.GetProductByParentCategoryAsync(query);
-    }
+        => await repo.GetProductByParentCategoryAsync(query);
+    
+    public async Task<Product> CreateAsync(Product product)
+        => await repo.CreateAsync(product);
+    
+    public async Task UpdateAsync(Product product)
+        => await repo.UpdateAsync(product);
+    
+    public async Task DeleteAsync(Guid id)
+        => await repo.DeleteAsync(id);
+    
+    
+    
 }
